7fcd7839b5870a1bf3a88e218d5e3c33c4292d45
wesleyornellas <wesleyornellas@outlook.com>
2021-01-26 12:31:55 -0300

    Adicionando projeto

---
+++ src/main/java/escola/aluno/Aluno.java
@@ -0,0 +1,36 @@
+package escola.aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//CLASSE VALUE ENTIDADE
+public class Aluno {
+
+    private CPF cpf;
+    private String nome;
+    private Email email;
+
+    private List<Telefone> telefones = new ArrayList<Telefone>();
+
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public void adicionarTelefone(String ddd, String numero) {
+        this.telefones.add(new Telefone(ddd, numero));
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+}
---
+++ src/main/java/escola/aluno/Email.java
@@ -0,0 +1,18 @@
+package escola.aluno;
+
+//CLASSE VALUE OBJECT
+public class Email {
+    private String endereco;
+
+    public  Email(String endereco){
+        if(endereco == null || !endereco.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$")){
+            throw  new IllegalArgumentException("E-mail invalido!");
+        }
+
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+}
